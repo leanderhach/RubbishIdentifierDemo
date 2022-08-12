3f30d569adfb978504431e9c04920ddd10bf820a
Leander Hach <leander.hach@gmail.com>
2022-08-13 01:15:54 +1200

    eslint

--- app/src/components/InteractableOverlay.vue
+++ app/src/components/InteractableOverlay.vue
@@ -360,17 +360,17 @@ main {
 }
 
 .play-button {
-  padding:10px 20px;
-  border-radius:20px;
-  border:none;
-  outline:none;
+  padding: 10px 20px;
+  border-radius: 20px;
+  border: none;
+  outline: none;
   background-color: #27ae60;
-  color:white;
-  position:absolute;
-  top:30px;
-  right:30px;
+  color: white;
+  position: absolute;
+  top: 30px;
+  right: 30px;
   font-family: "Source Sans Pro", sans-serif;
-  font-weight:bold;
+  font-weight: bold;
   font-weight: 1.25rem;
 }
 
